568e40f5f2b58982fd6914033369f4a96c573eec
NandoRunner <nando.az@gmail.com>
2020-09-29 16:54:41 -0300

    AppInfo GetTitle

---
+++ FAndradeTI.Util.WinForms/AppInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FAndradeTI.Util.WinForms
+{
+    public static class AppInfo
+    {
+        public static string GetFullTitle()
+        {
+            return $"{Assembly.GetCallingAssembly().GetCustomAttribute<AssemblyTitleAttribute>().Title}{new String(' ', 10)}v{Application.ProductVersion}{new String(' ', 10)}{Application.CompanyName}";
+        }
+
+        public static string GetTitle()
+        {
+            return $"{Assembly.GetCallingAssembly().GetCustomAttribute<AssemblyTitleAttribute>().Title}{new String(' ', 10)}v{Application.ProductVersion}";
+
+        }
+    }
+}
